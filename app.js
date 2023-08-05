673174e4a461e0f99cb9b09cf945535a99815d8b
j <javohir@yandex.ru>
2023-08-05 09:13:31 +0500

    mongoos connection

---
+++ app.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/magesDB");
+
+const BugsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    status: {
+        type: String,
+        require: true
+    },
+    steps: {
+        type: String,
+        require: true
+    }
+})
+
+const Bugs = new mongoose.model("Bugs", BugsSchema)
+
+const mage_1 = new Bugs({
+    name: "Takashi",
+
+
+});
+
+mage_1.save();
